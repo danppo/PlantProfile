8abc921a230eb329d96d50ffc760e5cdfc2f37cd
danppo <danielpocock@gmail.com>
2023-08-28 12:04:54 +0100

    initial app build

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PlantPage from './components/PlantPage';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import styles from './App.module.scss';
+//theme
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+
+//core
+import 'primereact/resources/primereact.min.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<PlantPage />}>
+          {/* <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
