dd0e9eb3cad0ddc896598587e2a8e8f55a78183d
danppo <danielpocock@gmail.com>
2023-09-03 08:39:38 +0100

    add title page

--- src/components/PlantPage/PlantPage.tsx
+++ src/components/PlantPage/PlantPage.tsx
@@ -3,6 +3,22 @@ import { Chip } from 'primereact/chip';
 
 const PlantPage = () => {
   return (
+    <>
+    <div className="grid grid-nogutter surface-0 text-800">
+    <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
+        <section>
+            <span className="block text-6xl font-bold mb-1">Create the screens</span>
+            <div className="text-6xl text-primary font-bold mb-3">your visitors deserve to see</div>
+            <p className="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+            <Button label="Learn More" type="button" className="mr-3 p-button-raised" />
+            <Button label="Live Demo" type="button" className="p-button-outlined" />
+        </section>
+    </div>
+    <div className="col-12 md:col-6 overflow-hidden">
+        <img src="/monstera.jpg" alt="hero-1" className="md:ml-auto block md:h-full" style={{ clipPath: 'polygon(8% 0, 100% 0%, 100% 100%, 0 100%)' }} />
+    </div>
+</div>
     <div className='surface-0'>
       <div className='font-medium text-3xl text-900 mb-3'>Movie Information</div>
       <div className='text-500 mb-5'>
@@ -55,6 +71,7 @@ const PlantPage = () => {
         </li>
       </ul>
     </div>
+    </>
   );
 };
 
